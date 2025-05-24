1dca6ef2475dabdb2ba65ad50450b15fe0cb94e0
SF78239 <shayanf17@gmail.com>
2025-05-24 13:02:35 +0530

    Delete load_transactions.py

--- load_transactions.py
+++
@@ -1,8 +0,0 @@
-import json
-import os
-
-def load_transactions(_input=None):
-    path = os.path.join(os.path.dirname(__file__), "transactions.json")
-    with open(path, "r") as f:
-        data = json.load(f)
-    return {"transactions": data}
